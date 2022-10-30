cc180154d4c1416c051643e299b0c3e4d5f7d5cc
tharukshi724 <tharukshiw@gmail.com>
2022-10-30 22:20:49 +0530

    db connection completed

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
-const express = require('express')
-const bodyparser = require('body-parser');
-const mongoose = require('mongoose')
+const express = require("express");
+const bodyparser = require("body-parser");
+const mongoose = require("mongoose");
 const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+
 //middleware usage
-app.use(bodyparser.json())
-app.use(bodyparser.urlencoded({extended:true}));
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
 
-app.get('/',(req,res)=>{
-    res.send("test")
-})
+
+app.get("/", (req, res) => {
+  res.send("test");
+});
 //database connection
-mongoose.connect();
 
-app.listen(3000);
+mongoose
+  .connect(process.env.DB_Cloud)
+  .then((_connect) => {
+    console.log(" Database connected Success ");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
+const port = process.env.PORT || 5080;
+app.listen(port, () => {
+  console.log("server run in port " + port);
+});
